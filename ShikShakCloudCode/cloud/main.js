38645cbb8159cf733e509d61f30994090dcc8c71
shakked <zshakked@stevens.edu>
2015-02-15 23:28:14 -0500

    Cloud code is sending push notifications to users who have a shakd that gets banned

--- ShikShakCloudCode/cloud/main.js
+++ ShikShakCloudCode/cloud/main.js
@@ -1,21 +1,42 @@
-
 Parse.Cloud.afterSave("ZSSShak", function(shak) {
-	var isBanned = shak.get("isBanned");
+	var deviceToken = shak.object.get("deviceToken");
+	var installationQuery = new Parse.Query(Parse.Installation);
+    installationQuery.equalTo("deviceToken", shak.object.get("deviceToken"));
+
+	var isBanned = shak.object.get("isBanned");
 	if (!isBanned) {
-		var reportCount = shak.get("reportCount");
+		var reportCount = shak.object.get("reportCount");
 		if (reportCount >= 7) {
-			shak.set("isBanned", true);
-			shak.save({
-				isBanned : true
+			shak.object.save({
+			  isBanned: true,
 			}, {
-				success: function(shak) {
-					//saved
-				},
-				error: function(shak, error) {
-					//save failed
-				}
-			});
+			  success: function(shak) {
+			  	Parse.Push.send({
+                        where: installationQuery,
+                        data:{
+                            alert: "One of your Shaks has been banned. Please follow our posting guidelines.",
+                            sound: "Sup.aiff",
+                            type : "message"
+                            }
+                        }); 
+			  	// installationQuery.find({
+				  // success: function(results) {
+				  //   // Do something with the returned Parse.Object values
+				  //   for (var i = 0; i < results.length; i++) { 
+				  //     var shak = results[i];
+				  //     shak.object.increment("bannedCount");
+				  //     shak.object.save();
+				  //   }
+				  // },
+				//   error: function(error) {
+
+				//   }
+				// });
+			  // },
+			  // error: function(shak, error) {
+
+			  // }
+			// });
 		}
 	}
-
-}
+});
