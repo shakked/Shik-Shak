c3d57b96cb1403b72ad6c0129ee638ba378731d8
shakked <zshakked@stevens.edu>
2015-02-17 11:08:52 -0500

    Added reporting functionality

--- ShikShakCloudCode/cloud/main.js
+++ ShikShakCloudCode/cloud/main.js
@@ -3,40 +3,64 @@ Parse.Cloud.afterSave("ZSSShak", function(shak) {
 	var installationQuery = new Parse.Query(Parse.Installation);
     installationQuery.equalTo("deviceToken", shak.object.get("deviceToken"));
 
+    Parse.Cloud.useMasterKey();
+
 	var isBanned = shak.object.get("isBanned");
 	if (!isBanned) {
 		var reportCount = shak.object.get("reportCount");
 		if (reportCount >= 7) {
-			shak.object.save({
-			  isBanned: true,
-			}, {
-			  success: function(shak) {
-			  	Parse.Push.send({
-                        where: installationQuery,
-                        data:{
-                            alert: "One of your Shaks has been banned. Please follow our posting guidelines.",
-                            sound: "Sup.aiff",
-                            type : "message"
-                            }
-                        }); 
-			  	// installationQuery.find({
-				  // success: function(results) {
-				  //   // Do something with the returned Parse.Object values
-				  //   for (var i = 0; i < results.length; i++) { 
-				  //     var shak = results[i];
-				  //     shak.object.increment("bannedCount");
-				  //     shak.object.save();
-				  //   }
-				  // },
-				//   error: function(error) {
+			shak.object.set("isBanned", true);
+			shak.object.save();
+			Parse.Push.send({
+                where: installationQuery,
+                data:{
+                    alert: "One of your Shaks has been banned. Please follow our posting guidelines.",
+                    sound: "Sup.aiff",
+                    type : "message"
+                    }
+            }); 
+            installationQuery.first({
+				  success: function(object) {
+				  		console.log("Query was successful");
+				  		var bannedCount = object.get("bannedCount");
+				  		console.log("Current bannedCount for installation: " + bannedCount);
+				  		if (!bannedCount) {
+				  			bannedCount = 1;
+				  			object.set("bannedCount", bannedCount);
+				  		} else {
+				  			object.set("bannedCount", ++bannedCount);
+				  		}
+				  		console.log("BannedCount after logic: " + bannedCount);
+				  		object.save();
+				  },
+				  error: function(error) {
+				  		console.log("Query failed");
+				  		console.log("Error: " + error.message);
+				  }
+			});
 
-				//   }
-				// });
-			  // },
-			  // error: function(shak, error) {
-
-			  // }
-			// });
 		}
+	}
+});
+
+
+Parse.Cloud.afterSave(Parse.Installation, function(installation) {
+	var bannedCount = installation.object.get("bannedCount");
+	if (bannedCount >= 3) {
+		installation.object.set("isBanned" , true);
+		installation.object.save();
+
+	var installationQuery = new Parse.Query(Parse.Installation);
+    installationQuery.equalTo("deviceToken", installation.object.get("deviceToken"));
+
+		Parse.Push.send({
+            where: installationQuery,
+            data:{
+                alert: "You have been banned from Shik Shak.",
+                sound: "Sup.aiff",
+                type : "message"
+                }
+        }); 
+
 	}
 });
