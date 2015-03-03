58079a7d367ec283ad633f243957b2f5e899ad2d
shakked <zshakked@stevens.edu>
2015-03-03 00:17:58 -0500

    Got rid of code leftover from playing with view

--- Shik Shak/ZSSThemeColorPickerController.m
+++ Shik Shak/ZSSThemeColorPickerController.m
@@ -40,8 +40,6 @@
 
 - (void)configureViews {
     [self configureButtons];
-    [self.sineWave setBackgroundColor:[UIColor clearColor]];
-
 }
 
 - (void)configureButtons {
@@ -50,10 +48,7 @@
     self.continueButton.layer.cornerRadius = 5.0f;
 }
 
-
-
 - (IBAction)colorButtonPressed:(id)sender {
-
     UIButton *buttonPressed = (UIButton *)sender;
     UIColor *themeColor = buttonPressed.backgroundColor;
     self.view.backgroundColor = themeColor;
@@ -61,8 +56,6 @@
     self.continueButton.hidden = NO;
     self.continueButton.titleLabel.textColor = themeColor;
 }
-
-
 
 - (IBAction)continueButtonPressed:(id)sender {
     UIColor *themeColor = self.lastPressedColor;
@@ -72,33 +65,10 @@
 }
 
 - (void)createAndConfigureUserWithThemeColor:(UIColor *)themeColor {
-
     ZSSUser *currentUser = [[ZSSLocalQuerier sharedQuerier] currentUser];
     currentUser.themeColor = themeColor;
     [[ZSSLocalStore sharedStore] saveCoreDataChanges];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - Animation Junk
-
-//CABasicAnimation* rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
-//rotationAnimation.toValue = @(3.14);
-//rotationAnimation.duration = 1.0f;
-//rotationAnimation.autoreverses = YES; // Very convenient CA feature for an animation like this
-//rotationAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-//[[self.sineWave layer] addAnimation:rotationAnimation forKey:@"revItUpAnimation"];
-
-//        [UIView animateWithDuration:0.4 delay:0.0 options:UIViewAnimationOptionRepeat|UIViewAnimationOptionCurveLinear animations:^{
-//
-//            self.sineWave.transform = CGAffineTransformMakeTranslation(self.view.frame.size.width / 2.0, 0);
-//        } completion:^(BOOL finished) {
-//            self.sineWave.transform = CGAffineTransformMakeTranslation(0, 0);
-//        }];
-
 
 
 @end
